8b4c14569080c27a3bb1204049a11c4012f01f5a
Gavin P <gavinjpeeler@gmail.com>
2026-01-10 16:18:19 -0800

    Cleared match scouting app

--- client/src/apps/match/MatchApp.tsx
+++ client/src/apps/match/MatchApp.tsx
@@ -1,63 +1,30 @@
-import EndgameButton from './components/EndGameButton'; 
-import FieldButton from './components/LeaveButton';
 import LinkButton from '../../components/LinkButton';
 import {
-    ClimbPosition,
     MatchData,
     MatchSchedule,
     RobotPosition,
     // ScouterPosition,
 } from 'requests';
-import { SetStateAction, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MaterialSymbol } from 'react-material-symbols';
 import 'react-material-symbols/rounded';
 import SignIn from '../../components/SignIn';
 import Dialog from '../../components/Dialog';
 import NumberInput from '../../components/NumberInput';
-import CheckBoxMatch from './components/CheckBoxButton';
 import { useStatus } from '../../lib/useStatus';
 import TeamDropdown from '../../components/TeamDropdown';
 import { useQueue } from '../../lib/useQueue';
 import scheduleFile from '../../assets/matchSchedule.json';
 import { usePreventUnload } from '../../lib/usePreventUnload';
 import ToggleButton from '../../components/LightVDarkMode';
-import CoralSectionButton from './components/CoralSectionButton';
 
 const schedule = scheduleFile as MatchSchedule;
 
 interface MatchScores {
-    autoL1: number;
-    autoL2: number;
-    autoL3: number;
-    autoL4: number;
-    autoAlgaenetRobot: number;
-    autoProcessor: number;
-    autoRemove: number;
-    teleRemove: number;
-    teleL1: number;
-    teleL2: number;
-    teleL3: number;
-    teleL4: number;
-    teleAlgaenetRobot: number;
-    teleProcessor: number;
-    teleRemoved: number;
+
 }
 const defaultScores: MatchScores = {
-    autoL1: 0,
-    autoL2: 0,
-    autoL3: 0,
-    autoL4: 0,
-    autoAlgaenetRobot: 0,
-    autoProcessor: 0,
-    autoRemove: 0,
-    teleRemove: 0,
-    teleL1: 0,
-    teleL2: 0,
-    teleL3: 0,
-    teleL4: 0,
-    teleAlgaenetRobot: 0,
-    teleProcessor: 0,
-    teleRemoved: 0
+
 };
 
 function MatchApp() {
@@ -65,33 +32,21 @@ function MatchApp() {
     const [sendQueue, sendAll, queue, sending] = useQueue();
     const [teamNumber, setTeamNumber] = useState<number>();
     const [matchNumber, setMatchNumber] = useState<number>();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [count, setCount] = useState<MatchScores>(defaultScores);
-    const [leave, setLeave] = useState(false); //false=Not Left, true=Left
     const [start1, setStart1] = useState(false);
     const [start2, setStart2] = useState(false);
     const [start3, setStart3] = useState(false);
-    const [source1, setSource1] = useState(false);
-    const [source2, setSource2] = useState(false);
-    const [ground1, setGround1] = useState(false);
-    const [ground2, setGround2] = useState(false);
-    const [ground3, setGround3] = useState(false);
-    const [deposit1, setDeposit1] = useState(false);
-    const [deposit2, setDeposit2] = useState(false);
-    const [deposit3, setDeposit3] = useState(false);
-    const [deposit4, setDeposit4] = useState(false);
-    const [deposit5, setDeposit5] = useState(false);
-    const [deposit6, setDeposit6] = useState(false);
     const [countHistory, setCountHistory] = useState<MatchScores[]>([]);
-    const [climbPosition, setClimbPosition] = useState<ClimbPosition>('none');
     const [showCheck, setShowCheck] = useState(false);
     const [scouterName, setScouterName] = useState('');
     const [robotPosition, setRobotPosition] = useState<RobotPosition>();
     const [toggleState, setToggleState] = useState(false);
     // const [scouterPosition, setScouterPosition] = useState<ScouterPosition>();
 
-    const blueAlliance = (
-        ['blue_1', 'blue_2', 'blue_3'] as (string | undefined)[]
-    ).includes(robotPosition);
+    // const blueAlliance = (
+    //     ['blue_1', 'blue_2', 'blue_3'] as (string | undefined)[]
+    // ).includes(robotPosition);
 
     const handleAbsentRobot = async () => {
         if (robotPosition == undefined || matchNumber == undefined) {
@@ -107,59 +62,23 @@ function MatchApp() {
                 matchNumber,
                 robotTeam: undefined,
             },
-            leftStartingZone: leave,
             startingZone: {
                 start1: start1,
                 start2: start2,
                 start3: start3,
-            },
-            pickupLocation: {
-                source1: source1,
-                source2: source2,
-                ground1: ground1,
-                ground2: ground2,
-                ground3: ground3
-            },
-            placement: {
-                deposit1: deposit1,
-                deposit2: deposit2,
-                deposit3: deposit3,
-                deposit4: deposit4,
-                deposit5: deposit5,
-                deposit6: deposit6,
-            },
-            autoCoral: {
-                L1: count.autoL1,
-                L2: count.autoL2,
-                L3: count.autoL3,
-                L4: count.autoL4,
-            },
-            autoAlgae: {
-                netRobot: count.autoAlgaenetRobot,
-                processor: count.autoProcessor,
-                remove: count.autoRemove
-            },
-            teleCoral: {
-                L1: count.teleL1,
-                L2: count.teleL2,
-                L3: count.teleL3,
-                L4: count.teleL4
-            },
-            teleAlgae: {
-                netRobot: count.teleAlgaenetRobot,
-                processor: count.teleProcessor,
-                remove: count.teleRemove
-            },
-            climb: climbPosition,
+            }
         };
 
         sendQueue('/data/match', data);
         setCount(defaultScores);
-        setClimbPosition('none');
-        setLeave(false);
         setMatchNumber(matchNumber + 1);
         setCountHistory([]);
 
+        // temporary, remove these later
+        setStart1(false);
+        setStart2(false);
+        setStart3(false);
+        
         setShowCheck(true);
 
         setTimeout(() => {
@@ -184,70 +103,15 @@ function MatchApp() {
                 matchNumber,
                 robotTeam: teamNumber,
             },
-            leftStartingZone: leave,
             startingZone: {
                 start1: start1,
                 start2: start2,
                 start3: start3,
             },
-            pickupLocation: {
-                source1: source1,
-                source2: source2,
-                ground1: ground1,
-                ground2: ground2,
-                ground3: ground3
-            },
-            placement: {
-                deposit1: deposit1,
-                deposit2: deposit2,
-                deposit3: deposit3,
-                deposit4: deposit4,
-                deposit5: deposit5,
-                deposit6: deposit6,
-            },
-            autoCoral: {
-                L1: count.autoL1,
-                L2: count.autoL2,
-                L3: count.autoL3,
-                L4: count.autoL4
-            },
-            autoAlgae: {
-                netRobot: count.autoAlgaenetRobot,
-                processor: count.autoProcessor,
-                remove: count.autoRemove
-            },
-            teleCoral: {
-               L1: count.teleL1,
-               L2: count.teleL2,
-               L3: count.teleL3,
-               L4: count.teleL4
-            },
-            teleAlgae: {
-                netRobot: count.teleAlgaenetRobot,
-                processor: count.teleProcessor,
-                remove: count.teleRemove
-            },
-            climb: climbPosition,
         };
         
         sendQueue('/data/match', data);
         setCount(defaultScores);
-        setClimbPosition('none');
-        setLeave(false);
-        setStart1(false);
-        setStart2(false);
-        setStart3(false);
-        setDeposit1(false);
-        setDeposit2(false);
-        setDeposit3(false);
-        setDeposit4(false);
-        setDeposit5(false);
-        setDeposit6(false);
-        setSource1(false);
-        setSource2(false);
-        setGround1(false);
-        setGround2(false);
-        setGround3(false);
         setMatchNumber(matchNumber + 1);
         setCountHistory([]);
 
@@ -273,10 +137,10 @@ function MatchApp() {
             setCount(countHistory.at(-1)!);
         }
     };
-    const handleSetCount = (newCount: SetStateAction<MatchScores>) => {
-        setCountHistory([...countHistory, count]);
-        setCount(newCount);
-    };
+    // const handleSetCount = (newCount: SetStateAction<MatchScores>) => {
+    //     setCountHistory([...countHistory, count]);
+    //     setCount(newCount);
+    // };
 
     useEffect(() => {
         setTeamNumber(
@@ -403,133 +267,14 @@ function MatchApp() {
                     Autonomous
                 </h2>
                 
-                <img src={`${blueAlliance ? 'bluesidematch.png': 'redsidematch.png'}`} width={500} height={600} className='justify-self-center my-7'></img>
-                {   <CheckBoxMatch
-                    checked={ground1}
-                    onChange={setGround1}
-                    className={`${blueAlliance ? 'top-[255px] left-[155px]' :'top-[265px] left-[335px] ' } absolute z-20 h-10 w-10 overflow-hidden rounded-full text-left`}>
-                    </CheckBoxMatch>
-                    // ground 1
-                    }
-                    <CheckBoxMatch
-                    checked={ground2}
-                    onChange={setGround2}
-                    className={`${blueAlliance ? 'top-[335px] left-[155px]' :'top-[365px] left-[340px]' } absolute z-20 h-10 w-10 overflow-hidden rounded-full text-left`}>
-                    </CheckBoxMatch>
-                    {/* ground 2 */}
-                    <CheckBoxMatch
-                    checked={ground3}
-                    onChange={setGround3}
-                    className={`${blueAlliance ? 'top-[415px] left-[155px]' :'top-[435px] left-[340px]' } absolute z-20 h-10 w-10 overflow-hidden rounded-full text-left`}>
-                    </CheckBoxMatch>
-                    {/* ground 3 */}
-                    <CheckBoxMatch
-                    checked={source1}
-                    onChange={setSource1}
-                    className={`${blueAlliance? 'top-[150px] left-[130px]' :'top-[175px] left-[345px] ' } absolute z-20 h-10 w-10 overflow-hidden rounded-full text-left`}>
-                    </CheckBoxMatch>
-                    {/* source 1 */}
-                    <CheckBoxMatch
-                    checked={source2}
-                    onChange={setSource2}
-                    className={`${blueAlliance? 'top-[510px] left-[130px]' :'top-[510px] left-[345px] ' } absolute z-20 h-10 w-10 overflow-hidden rounded-full text-left`}>
-                    </CheckBoxMatch>
-                    {/* source 2 */}
-                    <CheckBoxMatch
-                    checked={start1}
-                    onChange={setStart1}
-                    className={`${blueAlliance? 'top-[230px] left-[440px] ' :'top-[230px] left-[35px]' } absolute z-20 h-10 w-10 overflow-hidden rounded-full text-left`}>
-                    </CheckBoxMatch>
-                    {/* starting location 1 */}
-                    <CheckBoxMatch
-                    checked={start2}
-                    onChange={setStart2}
-                    className={`${blueAlliance? 'top-[330px] left-[440px]' :'top-[330px] left-[35px]' } absolute z-20 h-10 w-10 overflow-hidden rounded-full text-left`}>
-                    </CheckBoxMatch>
-                    {/* starting location 2 */}
-                    <CheckBoxMatch
-                    checked={start3}
-                    onChange={setStart3}
-                    className={`${blueAlliance? 'top-[430px] left-[440px]' :'top-[430px] left-[35px]' } absolute z-20 h-10 w-10 overflow-hidden rounded-full text-left`}>
-                    </CheckBoxMatch>
-                    {/* starting location 3 */}
-                    
-                                                                                
-                    
-                    <CoralSectionButton
-                        onChange={setDeposit1}
-                        value={deposit1} 
-                        className={`absolute ${blueAlliance? 'left-[19.5em] top-[19em]':'left-[12em] top-[20em]'} h-[1em] w-[4.5em] rotate-[30deg]`}
-                        selectClassName='bg-green-300'
-                        unselectClassName='bg-red-300'>
-                    </CoralSectionButton>
-                    <CoralSectionButton
-                        onChange={setDeposit2}
-                        value={deposit2} 
-                        className={`absolute ${blueAlliance? 'left-[21em] top-[21.5em]':'left-[13.5em] top-[23em]'} h-[1em] w-[4.5em] rotate-[90deg]`}
-                        selectClassName='bg-green-300'
-                        unselectClassName='bg-red-300'>
-                    </CoralSectionButton>
-                    <CoralSectionButton
-                        onChange={setDeposit3}
-                        value={deposit3} 
-                        className={`absolute ${blueAlliance? 'left-[19.5em] top-[24.5em]':'left-[12em] top-[26em]'} h-[1em] w-[4.5em] rotate-[-30deg]`}
-                        selectClassName='bg-green-300'
-                        unselectClassName='bg-red-300' >
-                    </CoralSectionButton>
-                    <CoralSectionButton 
-                        onChange={setDeposit4}
-                        value={deposit4}
-                        className={`absolute ${blueAlliance? 'left-[16em] top-[24.5em]':'left-[8.5em] top-[26em]'} h-[1em] w-[4.5em] rotate-[30deg]`}
-                        selectClassName='bg-green-300'
-                        unselectClassName='bg-red-300'>
-                    </CoralSectionButton>
-                    <CoralSectionButton 
-                        onChange={setDeposit5}
-                        value={deposit5}
-                        className={`absolute ${blueAlliance? 'left-[14.5em] top-[21.5em]':'left-[6.9em] top-[22.9em]'} h-[1em] w-[4.5em] rotate-[90deg]`}
-                        selectClassName='bg-green-300'
-                        unselectClassName='bg-red-300'>  
-                    </CoralSectionButton>
-                    <CoralSectionButton
-                        onChange={setDeposit6}
-                        value={deposit6} 
-                        className={`absolute ${blueAlliance? 'left-[16em] top-[19em]':'left-[8.5em] top-[20em]'} h-[1em] w-[4.5em] rotate-[-30deg]`}
-                        selectClassName='bg-green-300'
-                        unselectClassName='bg-red-300'>
-                    </CoralSectionButton>
-                    
-                <p
-                    className={`text-center m-5 ${toggleState ? 'text-white' : 'text-[#171c26]'}`}>
-                    Please select starting position, collection location, and scored location in autos.
-                </p>
-
-                { <FieldButton
-                    setCount={handleSetCount}
-                    setLeave={setLeave}
-                    count={count}
-                    teleOp={false}
-                    leave={leave}
-                    styleMode={toggleState}
-                /> }
                 <h2 className='my-6 mt-12 text-center text-5xl font-semibold text-green-600'>
                     Tele-Op
                 </h2>
-                { <FieldButton
-                    setCount={handleSetCount}
-                    count={count}
-                    teleOp={true}
-                    styleMode={toggleState}
-                /> }
+               
                 <h2 className='my-6 mt-12 text-center text-5xl font-semibold text-green-600'>
                     Endgame
                 </h2>
-                <EndgameButton
-                    climbPosition={climbPosition}
-                    setClimb={setClimbPosition}
-                    count={count}
-                    setCount={setCount}
-                />
+                
                 <div className='mb-5 mt-20 flex flex-col justify-center'>
                     <button
                         onClick={() => {
@@ -557,6 +302,6 @@ function MatchApp() {
     );
 }
 
-export type { MatchScores, ClimbPosition };
+export type { MatchScores };
 
 export default MatchApp;
--- requests/index.d.ts
+++ requests/index.d.ts
@@ -1,4 +1,4 @@
-export type ClimbPosition = 'shallow'| 'deep' | 'park' | 'none' | 'failed';
+export type ClimbPosition = 'level-1'| 'level-2' | 'level-3' | 'none' | 'failed';
 export type teamRoles = 'scoring' | 'defense' | 'support' | 'all-round';
 export type drivebase = 'tank' | 'swerve' | 'MECANUM' | 'other';
 
@@ -13,43 +13,27 @@ export type Foul =
     | 'insideRobot'
     | 'protectedZone'
     | 'pinning'
-    | 'multiplePieces'
-    | 'cageFoul'
     | 'other';
 export type Break = 'mechanismDmg' | 'batteryFall' | 'commsFail' | 'bumperFall';
 export type DefenseRank = 'fullDef' | 'someDef' | 'noDef';
 export type CommentValues =
     | 'great_driving'
     | 'good_driving'
-    | 'source_only'
-    | 'knock_pieces'
+    | 'outpost_only'
     | 'clogging'
-    | 'coral stuck'
-    | 'algae stuck'
     | 'effective_defense'
     | 'okay_defense'
     | 'ineffective_defense'
     | 'sturdy_build'
     | 'weak_build'
-    | 'avoids_under_stage';
-
-
-export type Net = boolean;
+    | 'avoids_under_trench';
 
 
 interface capabilities {
-    coral: boolean;
-    algae: boolean;
-    climbShallow: boolean;
-    climbDeep: boolean;
 }
 
 
 interface preference {
-    coralPrefer: boolean;
-    algaePrefer: boolean;
-    climbSPrefer: boolean;
-    climbDPrefer: boolean;
 }
 
 export type SuperPosition = 'red_ss' | 'blue_ss';
@@ -59,51 +43,10 @@ export type ScouterPosition = 'red_right' | 'blue_right';
 
 export interface MatchDataAggregations {
     _id: { teamNumber: number};
-    averageCoral: number;
-    averageAlgae: number;
-    averageRemove: number;
-    avgClimbRate: number;
-    totalL1: number;
-    totalL2: number;
-    totalL3: number;
-    totalL4: number;
-    totalCoral: number;
-    totalProcessor: number;
-    totalNet: number;
-    totalRemoved: number;
-    totalAlgae: number;
-    coralDrop1: boolean;
-    coralDrop2: boolean;
-    coralDrop3: boolean;
-    coralDrop4: boolean;
-    coralDrop5: boolean;
-    coralDrop6: boolean;
-    groundPick1: boolean;
-    groundPick2: boolean;
-    groundPick3: boolean;
-    sourcePick1: boolean;
-    sourcePick2: boolean;
-    start1: boolean;
-    start2: boolean;
-    start3: boolean;
 }
 
 export interface matchOutliersAggregation {
     _id: { teamNumber: number };
-    autoL1: number;
-    autoL2: number;
-    autoL3: number;
-    autoL4: number;
-    leave: number;
-    teleL1: number;
-    teleL2: number;
-    teleL3: number;
-    teleL4: number;
-    teleProcessor: number;
-    teleNet: number;
-    shallow: number;
-    deep: number;
-    park: number;
 }
 
 export interface SuperDataAggregations {
@@ -111,7 +54,6 @@ export interface SuperDataAggregations {
     avgFouls: number;
     maxFouls: number;
     humanAccuracy: number;
-
 }
 
 export interface SuperFoulAggregationsData{
@@ -119,34 +61,11 @@ export interface SuperFoulAggregationsData{
     totalInsideRobot: number;
     totalProtectedZone: number;
     totalPinning: number;
-    totalMultiplePieces: number;
-    totalCageFoul: number;
     totalOther: number;
 }
 
 export interface MatchIndividualDataAggregations {
     _id: { teamNumber: number, matchNumber: number, robotPosition: RobotPosition };
-    totalL1: number;
-    totalL2: number;
-    totalL3: number;
-    totalL4: number;
-    totalProcessor: number;
-    totalNet: number;
-    totalRemoved: number;
-    coralDrop1: boolean;
-    coralDrop2: boolean;
-    coralDrop3: boolean;
-    coralDrop4: boolean;
-    coralDrop5: boolean;
-    coralDrop6: boolean;
-    groundPick1: boolean;
-    groundPick2: boolean;
-    groundPick3: boolean;
-    sourcePick1: boolean;
-    sourcePick2: boolean;
-    start1: boolean;
-    start2: boolean;
-    start3: boolean;
 }
 
 export interface MetaData {
@@ -156,61 +75,19 @@ export interface MetaData {
     robotPosition: RobotPosition;
 }
 
-interface coral {
-    L1: number;
-    L2: number;
-    L3: number;
-    L4: number;
-}
-
-interface algae {
-    netRobot: number;
-    processor: number;
-    remove: number;
-}
-
-export interface netHuman {
-    Success: number;
-    Failed: number;
-}
-
 interface StartingZone {
     start1: boolean;
     start2: boolean;
     start3: boolean;
 }
 
-interface pickup {
-    source1: boolean;
-    source2: boolean;
-    ground1: boolean;
-    ground2: boolean;
-    ground3: boolean
-}
 
-interface placeLocation {
-    deposit1: boolean;
-    deposit2: boolean;
-    deposit3: boolean;
-    deposit4: boolean;
-    deposit5: boolean;
-    deposit6: boolean;
-}
 // - `POST` `/data/match`
 
 export interface MatchData {
     metadata: MetaData;
     // No competition info
-    leftStartingZone: boolean;
     startingZone: StartingZone;
-    pickupLocation: pickup;
-    placement: placeLocation;
-    autoCoral: coral;
-    autoAlgae: algae;
-    teleCoral: coral;
-    teleAlgae: algae;
-    climb: ClimbPosition;
-    // disabledSeconds: number;
 }
 
 // - `POST` `/data/super`
@@ -221,7 +98,6 @@ export interface SuperData {
     break: Record<Break, number>;
     defense: DefenseRank;
     defended: boolean;
-    humanShooter?: netHuman;
     comments: CommentValues[];
 }
 
@@ -239,7 +115,6 @@ export interface PitFile {
     scouterName: string;
     teamNumber: number;
     pitBatteryCount: number;
-    hopperIntake: boolean;
     comments: string;
     photo: string;
 }
